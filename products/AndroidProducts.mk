3607b95a04102a7e0090649bae547990ed067a0b
preludedrew <Andrew.Boren@gmail.com>
2011-01-13 19:16:22 -0700

    Add glacier to evervolv vendor.

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -2,4 +2,5 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/evervolv_inc.mk \
     $(LOCAL_DIR)/evervolv_supersonic.mk \
     $(LOCAL_DIR)/evervolv_heroc.mk \
+    $(LOCAL_DIR)/evervolv_glacier.mk \
  
---
+++ products/evervolv_glacier.mk
@@ -0,0 +1,50 @@
+# Inherit device configuration for glacier.
+$(call inherit-product, device/htc/glacier/glacier.mk)
+
+# Inherit some common Evervolv stuff.
+$(call inherit-product, vendor/evervolv/products/common.mk)
+
+# Include GSM stuff
+$(call inherit-product, vendor/evervolv/products/gsm.mk)
+
+PRODUCT_CODENAME := Evervolv-Spolio
+
+NIGHTLY := false
+
+# Set up the code name & Build version
+ifdef NIGHTLY
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.build.romversion=Evervolv-Spolio-v1.0.0-a0-$(shell date +%m%d%Y)-NIGHTLY-Supersonic \
+    	BUILD_VERSION := 1.0.1a1-$(shell date +%m%d%Y)-NIGHTLY
+else
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.build.romversion=Evervolv-Spolio-v1.0.0-a0-Supersonic \
+    	BUILD_VERSION := 1.0.1a1
+endif
+
+#
+# Setup device specific product configuration.
+#
+PRODUCT_NAME := evervolv_glacier
+PRODUCT_BRAND := tmobile
+PRODUCT_DEVICE := glacier
+PRODUCT_MODEL := HTC Glacier
+PRODUCT_MANUFACTURER := HTC
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_glacier BUILD_ID=FRG83 BUILD_DISPLAY_ID=GRH78 BUILD_FINGERPRINT=tmobile/htc_glacier/glacier/glacier:2.2.1/FRG83/277036:user/release-keys PRIVATE_BUILD_DESC="1.17.531.2 CL277036 release-keys"
+
+
+# Extra glacier overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/evervolv/overlay/glacier
+
+#Include Torch
+PRODUCT_PACKAGES += Torch
+
+# Broadcom FM radio
+#$(call inherit-product, vendor/evervolv/products/bcm_fm_radio.mk)
+
+
+#
+# Copy passion specific prebuilt files
+#
+PRODUCT_COPY_FILES +=  \
+    vendor/evervolv/prebuilt/hdpi/media/bootanimation.zip:system/media/bootanimation.zip
